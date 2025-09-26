4ee2c6ab691ddbd167425c59ba07d371b40b94a2
Neurodesk User <user@neurodesk.org>
2025-09-26 20:20:25 +0000

    fixed run_pipeline.sh

--- run_pipleline.sh
+++ run_pipleline.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 print_yellow() {
-    echo -e "==> \033[1;33m$1\033[0m <=="
+    echo -e "====> \033[1;33m$1\033[0m <===="
 }
 
 print_yellow "Starting Pipeline"
@@ -12,17 +12,19 @@ chmod +x Mriqc.sh
 chmod +x Preprocessing.sh
 chmod +x Analysis.sh
 
-raw_dir=MICROSTRUCTURE_M
-deriv_dir=ORIG_DATA
+raw_dir="$MICROSTRUCTURE_M"/raw
+deriv_dir="$ORIG_DATA"/derivatives
 
 print_yellow "Running Data Structure and Validating Data"
 ./data_structure_and_validation.sh
 
 print_yellow "Running MRIQC"
-./Mriqc.sh
+./Mriqc.sh "$raw_dir" "$deriv_dir"
 
 print_yellow "Running Preprocessing"
-./Preprocessing.sh
+./Preprocessing.sh "$raw_dir" "$deriv_dir"
 
 print_yellow "Running Analysis"
-./Analysis.sh
+./Analysis.sh "$deriv_dir"
+
+print_yellow "Pipeline Completed Successfully"
